618d7cfa7a2d4423edbc619dc39f1400737b5747
Hugo Blanchard <zaturgo@gmail.com>
2021-01-05 18:59:34 +0100

    Added classic function and main logic

--- main.py
+++ main.py
@@ -1,31 +1,41 @@
 # coding=utf-8
-
-#Il existe des algorithmes plus puissants en terme de complexité pour la multiplication de deux matrices (Strassen: O(n2.807)):
-#Le coppersmith winograd a une complexité de O(n2.3737) tandis qu'un algorithme de Josh Alman atteint une complexité de O(n2.3728596) en décembre 2020.
-
 import numpy as np
 import time
+def classic(a,b,c,dim):
+    for i in range(dim):
+        for j in range(dim):
+            c[i][j] = 0
+            for k in range(dim):
+                c[i][j] += a[i][k]*b[k][j]
+    return c;
+timeClassic = 0
+timeStrassen = 1
+k = 0;
+while timeClassic<timeStrassen:
+    k = k+1
+    print("--------------------------")
+    print("Execution pour matrice de taille "+str(2**k)+" (k="+str(k)+")")
+    a = []
+    b = []
+    c = []
+    for i in range(2**k):
+        a.append(np.random.randint(-9,9,2**k))
+        b.append(np.random.randint(-9,9,2**k))
+        c.append(np.random.randint(-9,9,2**k))
+    a = np.array(a);
+    b = np.array(b);
+    c = np.array(b);
+    start = time.perf_counter()
+    classic(a,b,c,2**k)
+    end = time.perf_counter()
+    print(f"L'exécution classique a pris {end - start:0.6f} secondes")
+    timeClassic = end - start
+    #start = time.perf_counter()
+    #strassen(a,b)
+    #end = time.perf_counter()
+    #print(f"L'exécution de strassen a pris {end - start:0.4f} secondes")
+    #timeStrassen = end - start
+    timeStrassen = timeClassic-1
+print("----------------------------")
+print("Strassen est plus rapide à partir de k="+str(k))
 
-def inputNumber(message):
-  while True:
-    try:
-       userInput = int(input(message))
-    except ValueError:
-       print("Veuillez entrer un entier")
-       continue
-    else:
-       return userInput
-       break
-k=inputNumber("Entrez une taille de matrice (2^k)");
-k = 2**k
-a = []
-b = []
-for i in range(k):
-    a.append(np.random.randint(-9,9,k))
-    b.append(np.random.randint(-9,9,k))
-
-a = np.array(a);
-b = np.array(b);
-start = time.perf_counter()
-end = time.perf_counter()
-print(f"L'exécution a pris {end - start:0.4f} secondes")
