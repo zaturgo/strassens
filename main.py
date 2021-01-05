4913d4bc7778c3258e92e63912c26643930f3e1c
Hugo Blanchard <zaturgo@gmail.com>
2021-01-05 14:50:22 +0100

    Initialising matrixes

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 # coding=utf-8
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
 import numpy as np
+def inputNumber(message):
+  while True:
+    try:
+       userInput = int(input(message))
+    except ValueError:
+       print("Not an integer! Try again.")
+       continue
+    else:
+       return userInput
+       break
+k=inputNumber("Entrez une taille de matrice (k)");
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print("Hi, {0}".format(name))  # Press ⌘F8 to toggle the breakpoint.
 
+a = []
+b = []
+for i in range(k):
+    a.append(np.random.randint(-9,9,k))
+    b.append(np.random.randint(-9,9,k))
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-
-a = np.array([[1, 2, 3], [4, 5, 6]])
+a = np.array(a);
+b = np.array(b);
+print(a)
+print(b)
